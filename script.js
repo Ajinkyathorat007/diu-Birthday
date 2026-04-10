7f873bfc396466fc48c3218f752a0fafa1b3a3b5
Ajinkyathorat007 <ajinkyat56@gmail.com>
2026-04-10 12:40:58 +0530

    Refactor script.js to improve readability and structure

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function goToScreen(id) {
 function checkAnswer() {
     const ans = answerInput.value.trim().toLowerCase();
 
-    if (ans === "AD") {
+    if (ans === "ad") {
         lockMsg.textContent = "Unlocked successfully 💖";
         playMusic();
         setTimeout(() => startCountdown(), 700);
